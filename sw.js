dab249b04010dd2c87633eb1ff476e8ed3614153
AmedeoSpinaMd <amedeo.spina.mv@gmail.com>
2026-03-28 19:38:20 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'aorta-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './sw.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
